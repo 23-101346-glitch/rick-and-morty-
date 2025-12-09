a3209b55b48cc75ee2ecc4bcf7f2e010cd84bcd0
Janna khaled <23-101346@students.eui.edu.eg>
2025-12-09 13:52:23 +0200

    last push

--- script.cs
+++ script.cs
@@ -8,3 +8,13 @@
   // Add the class to the div
   characterDiv.classList.add(randomCharacter);
 
+// Select the "Intro" button
+  const introBtn = document.querySelector(".navbt h3:contains('Intro')") || document.querySelectorAll(".navbt")[0];
+
+  // Select the target section
+  const target = document.querySelector(".s2div1");
+
+  // Add click event
+  introBtn.addEventListener("click", function() {
+    target.scrollIntoView({ behavior: "smooth" });
+  });
